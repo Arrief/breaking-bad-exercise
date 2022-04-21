29b397a757529b6d62a5951f271a19879843f1df
Arne Rief <arne-rief_student2022@wilder.school>
2022-04-21 14:59:26 +0200

    refactored version with Marc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-// 1. import API data with useEffect to display on page load
-// 2. call API
-// 3. loading text
-// 4. show character name, the nickname, the picture and the status for each character
 
 function App() {
   let [apiData, setApiData] = useState([]);
   let [loadedApi, setLoadedApi] = useState(false);
   let [survivors, setSurvivors] = useState(false);
-  let [copyOfOriginal, setCopyOfOriginal] = useState([]);
 
   useEffect(() => {
     fetch("https://www.breakingbadapi.com/api/characters")
     .then((response) => response.json())
     .then((data) => {
       setApiData(data);
-      setCopyOfOriginal(data);
       setLoadedApi(true);
       console.log(data);
     })
@@ -33,33 +27,37 @@ function App() {
 
   const handleAlive = () => {
     setSurvivors(!survivors)
-    if (survivors === false) {
-      let onlyAlive = apiData.filter((element) => element.status === "Alive");
-      setApiData(onlyAlive)
-    } else {
-      setApiData(copyOfOriginal);
-    }
-    console.log(survivors);
   }
 
   return (
     <div className="list-of-characters">
       <button onClick={handleAlive}>Show everybody who is alive</button>
-        {loadedApi == true
-          ? apiData.map((element, index) => (
-          <div className="char-wrapper" key={index}>
-          <div><img src={element.img}></img></div>
-          <div>{element.name}</div>
-          <div>{element.nickname}</div>
-          <div>{element.status}</div>
-          {element.status === "Alive" 
-            ? <button onClick={() => handleClick(element.char_id)}>Kill</button>
-            : null
-          }
-            </div>
-          ))
+        {loadedApi === true
+          ? survivors === false
+            ? apiData.map((element, index) => (
+             <div className="char-wrapper" key={index}>
+                <img src={element.img} alt={element.name}/>
+                <h1>{element.name}</h1>
+                <h3>{element.nickname}</h3>
+                <p>{element.status}</p>
+                {element.status === "Alive" &&
+                  <button onClick={() => handleClick(element.char_id)}>Kill</button>
+                }
+              </div>
+              ))
+            :  apiData.filter((element) => element.status === "Alive").map((element, index) => (
+              <div className="char-wrapper" key={index}>
+                 <img src={element.img} alt={element.name}/>
+                 <h1>{element.name}</h1>
+                 <h3>{element.nickname}</h3>
+                 <p>{element.status}</p>
+                 {element.status === "Alive" &&
+                   <button onClick={() => handleClick(element.char_id)}>Kill</button>
+                 }
+               </div>
+               ))
           : <p>Data loading...</p>
-      }
+        }
     </div>
   );
 }
