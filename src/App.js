096b8057ee6de8fa724a6271fcf48b238e3afb2f
Arne Rief <arne-rief_student2022@wilder.school>
2022-04-21 12:32:14 +0200

    group solution

--- src/App.js
+++ src/App.js
@@ -9,29 +9,56 @@ import './App.css';
 function App() {
   let [apiData, setApiData] = useState([]);
   let [loadedApi, setLoadedApi] = useState(false);
+  let [survivors, setSurvivors] = useState(false);
+  let [copyOfOriginal, setCopyOfOriginal] = useState([]);
 
   useEffect(() => {
     fetch("https://www.breakingbadapi.com/api/characters")
     .then((response) => response.json())
     .then((data) => {
       setApiData(data);
+      setCopyOfOriginal(data);
       setLoadedApi(true);
       console.log(data);
     })
   }, [])
 
+  const handleClick = (charId) => {
+    let index = apiData.findIndex((element) => element.char_id === charId);
+    let copyArray = [...apiData];
+    copyArray[index].status = "Deceased";
+    setApiData(copyArray);
+    console.log(apiData);
+  }
+
+  const handleAlive = () => {
+    setSurvivors(!survivors)
+    if (survivors === false) {
+      let onlyAlive = apiData.filter((element) => element.status === "Alive");
+      setApiData(onlyAlive)
+    } else {
+      setApiData(copyOfOriginal);
+    }
+    console.log(survivors);
+  }
+
   return (
-    <div className="App">
-      {loadedApi == true
-        ? apiData.map((element, index) => (
-          <>
-           <div key={index}><img src={element.img}></img></div>
-           <div key={index}>{element.name}</div>
-           <div key={index}>{element.nickname}</div>
-           <div key={index}>{element.status}</div>
-          </>
-        ))
-        : <p>Data loading...</p>
+    <div className="list-of-characters">
+      <button onClick={handleAlive}>Show everybody who is alive</button>
+        {loadedApi == true
+          ? apiData.map((element, index) => (
+          <div className="char-wrapper" key={index}>
+          <div><img src={element.img}></img></div>
+          <div>{element.name}</div>
+          <div>{element.nickname}</div>
+          <div>{element.status}</div>
+          {element.status === "Alive" 
+            ? <button onClick={() => handleClick(element.char_id)}>Kill</button>
+            : null
+          }
+            </div>
+          ))
+          : <p>Data loading...</p>
       }
     </div>
   );
