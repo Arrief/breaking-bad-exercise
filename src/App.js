67d6d9885f6d6bf451f29ca9ff48fb92d49b3bbf
Arne Rief <arne-rief_student2022@wilder.school>
2022-04-21 10:08:55 +0200

    API data is displayed now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+// 1. import API data with useEffect to display on page load
+// 2. call API
+// 3. loading text
+// 4. show character name, the nickname, the picture and the status for each character
+
 function App() {
+  let [apiData, setApiData] = useState([]);
+  let [loadedApi, setLoadedApi] = useState(false);
+
+  useEffect(() => {
+    fetch("https://www.breakingbadapi.com/api/characters")
+    .then((response) => response.json())
+    .then((data) => {
+      setApiData(data);
+      setLoadedApi(true);
+      console.log(data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loadedApi == true
+        ? apiData.map((element, index) => (
+          <>
+           <div key={index}><img src={element.img}></img></div>
+           <div key={index}>{element.name}</div>
+           <div key={index}>{element.nickname}</div>
+           <div key={index}>{element.status}</div>
+          </>
+        ))
+        : <p>Data loading...</p>
+      }
     </div>
   );
 }
